27d46d120c0bea4d3c6764927ee94130d5d92c56
shenjiayee <shenjiayee@outlook.com>
2021-06-30 08:43:45 +0800

    init

---
+++ src/test/java/com/shenjiayee/sanguosha/SanguoshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenjiayee.sanguosha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanguoshaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
